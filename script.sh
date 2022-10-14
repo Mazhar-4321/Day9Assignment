38223ec0c0dfaef96acd8771820f69281b2041b9
Mazhar-4321 <syedmazharali742@gmail.com>
2022-10-14 21:58:39 +0530

    [UC3] added part time employee and wage

--- script.sh
+++ script.sh
@@ -2,10 +2,12 @@ echo "Welcome To Employee Wage Computation"
 attendance=$((RANDOM%2))
 wagePerHour=20
 workingHours=8
+partTimeHours=8
 if [[ $attendance -eq 0 ]]
 then
 echo "Absent"
 else
 echo "Present"
 echo "Employee Wage:$(($wagePerHour*$workingHours))"
+echo "Part Time Wage:$(($partTimeHours*$wagePerHour))"
 fi
