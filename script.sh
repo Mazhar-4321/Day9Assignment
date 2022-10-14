382c718f533d677f3c412e2955c106f365b79858
Mazhar-4321 <syedmazharali742@gmail.com>
2022-10-14 21:52:46 +0530

    [UC2] added code for daily employee wage

--- script.sh
+++ script.sh
@@ -1,8 +1,11 @@
 echo "Welcome To Employee Wage Computation"
 attendance=$((RANDOM%2))
+wagePerHour=20
+workingHours=8
 if [[ $attendance -eq 0 ]]
 then
 echo "Absent"
 else
 echo "Present"
+echo "Employee Wage:$(($wagePerHour*$workingHours))"
 fi
