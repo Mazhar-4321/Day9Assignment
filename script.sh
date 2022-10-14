a3660347fab4b9a2a48ddbf6a4b3c6d5085a1657
Mazhar-4321 <syedmazharali742@gmail.com>
2022-10-14 22:02:37 +0530

    [UC4] refactored code from if-else to switch

--- script.sh
+++ script.sh
@@ -3,11 +3,21 @@ attendance=$((RANDOM%2))
 wagePerHour=20
 workingHours=8
 partTimeHours=8
-if [[ $attendance -eq 0 ]]
-then
-echo "Absent"
-else
-echo "Present"
-echo "Employee Wage:$(($wagePerHour*$workingHours))"
-echo "Part Time Wage:$(($partTimeHours*$wagePerHour))"
-fi
+case $attendance in
+	0)
+	echo "Absent"
+	;;
+	1)
+	echo "Present"
+	echo "Employee Wage:$(($wagePerHour*$workingHours))"
+	echo "Part Time Wage:$(($partTimeHours*$wagePerHour))"
+	;;
+esac
+##if [[ $attendance -eq 0 ]]
+##then
+##echo "Absent"
+##else
+##echo "Present"
+##echo "Employee Wage:$(($wagePerHour*$workingHours))"
+##echo "Part Time Wage:$(($partTimeHours*$wagePerHour))"
+##fi
