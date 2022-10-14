b71570140ee2e7adf6add33c59f3270fa62f0e53
Mazhar-4321 <syedmazharali742@gmail.com>
2022-10-14 22:08:17 +0530

    [UC5] added code for wages for a month

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@ attendance=$((RANDOM%2))
 wagePerHour=20
 workingHours=8
 partTimeHours=8
+daysPerMonth=20
 case $attendance in
 	0)
 	echo "Absent"
@@ -11,6 +12,7 @@ case $attendance in
 	echo "Present"
 	echo "Employee Wage:$(($wagePerHour*$workingHours))"
 	echo "Part Time Wage:$(($partTimeHours*$wagePerHour))"
+	echo "Per Month Wage:$(($workingHours*$wagePerHour*$daysPerMonth))"
 	;;
 esac
 ##if [[ $attendance -eq 0 ]]
